f438404bead8e64bb59d1ae40378d2a27e70e0a6
Surya Sah <158431545+SuryaSahh@users.noreply.github.com>
2025-01-27 16:59:56 +0000

    Memory

--- 01_Basics/05_datatype-summary.js
+++ 01_Basics/05_datatype-summary.js
@@ -1,4 +1,4 @@
-// Data is stored and accessed in the momory in two ways
+// Data is stored and accessed in the memory in two ways
 
 // 1. Primitive : call by value(change in the copy of data)
 // 7 types : string, Number, Boolean, null, undefined, symbol, BigInt
@@ -25,3 +25,25 @@ const myFunction = function () {
 console.log(typeof heros) // "object"
 console.log(typeof myObj) // "object"
 console.log(typeof myFunction) // "function"
+
+
+// ********************* Memory *****************
+// stack(primitive)=> copy
+// Heap(non-primitive)=> reference
+
+// stack : copy of the data
+let name="surya"
+let anothername=name
+anothername = "fire"
+console.log(name)
+console.log(anothername)
+
+// heap : reference of the data
+let userone={
+    email : "123@gmail.com",
+    age : 23
+}
+let usertwo = userone
+usertwo.email="abc@gmail.com"
+console.log(userone.email)
+console.log(usertwo.email)
