8e9373845cb6c71d8c60d24d696aa7bd2711fa39
Surya Sah <158431545+SuryaSahh@users.noreply.github.com>
2025-01-27 12:48:12 +0000

    operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -16,3 +16,27 @@ console.log(booleanOfNum)
 // 1 => true; 0 => false
 // "" => false
 // "surya" => true
+
+// *****Operations*****
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log(2+2)
+console.log(2**3)
+
+let str1 = "hello"
+let str2 = "surya"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2 ) /*122*/
+console.log(1 + 2 + "2") /*32*/
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
