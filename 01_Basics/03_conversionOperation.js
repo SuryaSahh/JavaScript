b6d72c35046c2a36104dd36aabc70325ee1e33b7
Surya Sah <158431545+SuryaSahh@users.noreply.github.com>
2025-01-27 12:10:37 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let scope = "99"
+console.log(typeof scope)
+console.log(typeof (scope))
+let ValueInNum=Number(scope)
+console.log(typeof ValueInNum)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefined => NaN
+
+let num=1;
+let booleanOfNum=Boolean(num)
+console.log(booleanOfNum)
+
+// 1 => true; 0 => false
+// "" => false
+// "surya" => true
