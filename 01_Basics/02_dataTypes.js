cd65e5b978e6a5f707def847b430905f50400d33
Surya Sah <158431545+SuryaSahh@users.noreply.github.com>
2025-01-27 11:44:59 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; /*treat all js code as newer version*/
+// alert(3+3) we are using nodejs not browser
+
+// string
+// number => 2 to power 53
+// boolean
+// bigint
+// null => standalone value
+// undefined => not assigned
+// symbol => unique
+// object
+
+console.log(typeof null) /*object*/
+console.log(typeof undefined) /*undefined*/
