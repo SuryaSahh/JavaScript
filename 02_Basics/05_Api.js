05afbeb9e369fac74c4f15c392aff0bc0163d4dd
Surya Sah <158431545+SuryaSahh@users.noreply.github.com>
2025-01-31 07:48:00 +0000

    Basic API

---
+++ 02_Basics/05_Api.js
@@ -0,0 +1,23 @@
+// Json format
+// API call in Object format
+{
+    "Name": "Surya",
+    "Roll": 21053331
+}
+
+
+// API call in Array format
+[
+    {
+        "Name": "Surya",
+        "Roll": 21053331
+    },
+    {
+        "Name": "Amrita",
+        "Roll": 2101967
+    }
+]
+
+
+
+
