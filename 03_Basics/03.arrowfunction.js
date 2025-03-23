4b370305949764e1028b800678a8f9848f5d2446
Surya Sah <158431545+SuryaSahh@users.noreply.github.com>
2025-03-23 17:02:39 +0000

    arrowfunction

--- 03_Basics/03.arrowfunction.js
+++ 03_Basics/03.arrowfunction.js
@@ -39,3 +39,25 @@ const chai3=()=> {
     console.log(this.username) //undefined
 }
 chai3()
+
+
+// Methods using Arrowfunction
+// 1st
+const addTwo=(n1,n2) =>{  //parameter
+    return n1+n2   // explicit return
+}
+
+// 2nd
+const addTwo2 = (n1,n2) => n1 + n2  //implicit return
+// or
+
+const addTwo3 = (n1,n2) => (n1 + n2)
+console.log(addTwo3(3,4))
+
+// Note:
+// {} => need return 
+// () => no need of return
+// this method is specially use for return object
+const addTwo4 = (n1,n2) => (
+    {username : "surya"}  // always need '()' paranthesis to return object
+)
