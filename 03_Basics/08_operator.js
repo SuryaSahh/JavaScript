b309bfb18de3a1ca3245aaf55766c5155253b215
Surya Sah <158431545+SuryaSahh@users.noreply.github.com>
2025-03-24 15:21:59 +0000

    operator

---
+++ 03_Basics/08_operator.js
@@ -0,0 +1,10 @@
+// Nullish Coalescing Operator (??) : null undefined
+let val;
+val = 5 ?? 10  //5
+val = null ?? 10 //10
+val = undefined ?? 15  //15
+val = null ?? 10 ?? 20  //10
+
+
+// terniary operator
+// condition ? true : false
