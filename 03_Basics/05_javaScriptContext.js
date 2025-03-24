f6108b7b13c05c29dce46288968d975006df7549
Surya Sah <158431545+SuryaSahh@users.noreply.github.com>
2025-03-24 14:38:32 +0000

    Execution Context

---
+++ 03_Basics/05_javaScriptContext.js
@@ -0,0 +1,14 @@
+// ************Interview***************
+// javascript is single threaded
+
+// {} => global execution context <= this
+// 1. Global EC
+// 2. Function EC
+// 3. Eval EC (not needed now)
+
+// {} => Memory creation phase  //memory allocation
+
+// steps:
+// 1. global execution/environment
+// 2. Memory phase 
+// 3. Execution phase
