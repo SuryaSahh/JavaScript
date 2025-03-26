b80290828dd14ee467ab2bda01df04ccb620bea3
Surya Sah <158431545+SuryaSahh@users.noreply.github.com>
2025-03-26 18:21:11 +0000

    forEach loop

---
+++ 03_Basics/11_forEachLoop.js
@@ -0,0 +1,45 @@
+// call back function doesn't have name
+
+// for each loop => highly order function
+const coding = ["js","ruby","java","python"]
+coding.forEach(function(item){
+    console.log(item)
+})
+
+// using arrow function
+coding.forEach((item)=>{
+    console.log(item)
+})
+
+// using name function 
+coding.forEach(function print(item){
+    console.log(item)
+})
+
+// interesting******************************
+coding.forEach((item,index,arr)=>{
+    console.log(item,index,arr)
+})
+
+
+// Array of object
+const coding1=[
+    {
+        name : "javascript",
+        fname : "js"
+    },
+    {
+        name : "java",
+        fname : "java"
+    },
+    {
+        name : "python",
+        fname : "py"
+    }
+]
+
+coding1.forEach((item)=>{
+    console.log(item.name)
+})
+
+// {} => item
